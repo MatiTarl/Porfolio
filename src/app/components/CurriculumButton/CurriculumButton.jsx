112f7121a65e53cc6f1f7571295b63d57406d358
Mati Tari <Matiastari@outlook.com.ar>
2024-07-31 18:42:13 -0300

    CORRECION

--- src/app/components/CurriculumButton/CurriculumButton.jsx
+++ src/app/components/CurriculumButton/CurriculumButton.jsx
@@ -4,17 +4,13 @@ import { saveAs } from "file-saver";
 
 export default function CurriculumButton() {
 
-  // const DescargarPDF = () => {
-  //   saveAs("", "./CurriculumMatias.pdf");
-  // };
-
   const DescargarPDF = () => {
-    const pdfUrl = "./CurriculumMatias.pdf"; // Ruta relativa al archivo PDF
-    window.open(pdfUrl, "_blank");
+    saveAs("", "./CurriculumMatias.pdf");
   };
 
   return (
     <div className="text-white w-full h-auto flex justify-center p-4">
+      <a href="/CurriculumMatias.pdf" download="Curriculum Matias Tari" className="w-50 w-40 bg-slate-100"></a>
   <button onClick={DescargarPDF} className="w-auto px-4 bg-zinc-200 h-10 flex rounded-3xl items-center" >
     <div className="pr-2">
       <Download />
