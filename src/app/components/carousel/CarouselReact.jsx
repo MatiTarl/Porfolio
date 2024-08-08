72ed2a0eb5852b8740674b94a227a59b9063c048
Mati Tari <Matiastari@outlook.com.ar>
2024-08-08 16:45:37 -0300

    Correcion de carousel y agregado de animaciones para cada card sin terminar

--- src/app/components/carousel/CarouselReact.jsx
+++ src/app/components/carousel/CarouselReact.jsx
@@ -1,74 +1,94 @@
+"use client";
+
 import { useState } from "react";
+//-----------------ICONS-----------------
 import Right from "../../../ui/icons/right";
 import Left from "../../../ui/icons/left";
-import Image from "next/image";
+//-----------------ICONS-----------------
+//-----------------Carousel-----------------
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+//-----------------Carousel-----------------
 
 export default function Carousel() {
   let slides = [
-    "/EcconomyPage.png",
-    "/GastifyPage.png",
-    "/abogadoPage.png"
+    {
+      href: "",
+      img: "/EcconomyPage.png",
+    },
+    {
+      href: "",
+      img: "/GastifyPage.png",
+    },
+    {
+      href: "",
+      img: "/abogadoPage.png",
+    },
+    {
+      href: "",
+      img: "/GastifyPage.png",
+    },
   ];
-  let [current, setCurrent] = useState(0);
-
-  let previousSlide = () => {
-    if (current === 0) setCurrent(slides.length - 1);
-    else setCurrent(current - 1);
-  };
-
-  let nextSlide = () => {
-    if (current === slides.length - 1) setCurrent(0);
-    else setCurrent(current + 1);
-  };
 
   return (
-    <section>
-      <div className="flex flex-col justify-center items-center ">
-      <h2 class="text-4xl text-white pb-10">Proyectos</h2>
-        <div className="overflow-hidden relative shadow-[0px_10px_107px_5px_rgba(253,_253,_253,_0.2)] rounded-3xl sm:w-[800px]">
-          <a href="https://google.com.ar">
-            <div
-              className={`flex transition ease-out duration-700 hs-carousel-body`}
-              style={{
-                transform: `translateX(-${current * 100}%)`,
-              }}
-            >
-              {slides.map((slide) => {
-                return <img src={slide} alt="Slides de proyectos" key="" />;
-              })}
-            </div>
-          </a>
-        </div>
-        <div className=" flex sm:absolute w-full justify-center sm:justify-between items-center text-white pt-5 sm:pt-0 px-2 sm:px-10 text-3xl">
-          <button
-            onClick={previousSlide}
-            className="relative overflow-hidden rounded-md px-5 py-2.5 text-white transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110"
-          >
-            <Left />
-          </button>
-          <button
-            onClick={nextSlide}
-            className="relative overflow-hidden rounded-md px-5 py-2.5 text-white transition-all duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:-translate-y-1 active:scale-x-90 active:scale-y-110"
-          >
-            <Right />
-          </button>
-        </div>
+    <section className="container">
+      <div className="items-center flex flex-col text-center pb-10">
+        <h2 className="text-4xl text-white p-3">Proyectos</h2>
+        <h5 className="text-white">Realizados</h5>
       </div>
-      <div className="hidden sm:flex bottom-0 sm:py-12 justify-center gap-3 w-full">
-        {slides.map((s, i) => {
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: "swiper-pagination", clickable: true }}
+        navigation={{
+          nextEl: "swiper-button-next",
+          prevEl: "swiper-button-prev",
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="relative items-center justify-center h-[600px] hover:animate-desplazarArriba animate-volverAbajo"
+      >
+        {slides.map((slide) => {
           return (
-            <div
-              onClick={() => {
-                setCurrent(i);
-              }}
-              key={"circle" + i}
-              className={`rounded-full w-5 h-5 cursor-pointer duration-700 ${
-                i == current ? "bg-white" : "bg-gray-500"
-              }`}
-            ></div>
+            <>
+              <SwiperSlide className="sm:max-w-[30rem] ">
+                <div className="sm:max-w-[30rem] ">
+                  <a href="https://www.google.com.ar">
+                    <img
+                      src={slide.img}
+                      className="rounded-xl sm:max-w-[30rem] hover:blur-[1px] hover: transition duration-700 "
+                      alt="Slides de proyectos"
+                    />
+                  </a>
+                </div>
+              </SwiperSlide>
+            </>
           );
         })}
-      </div>
+
+        {/* <div className="relative pb-8 flex items-center justify-center">
+          <div className="swiper-button-prev">
+            <Right></Right>
+          </div>
+          <div className="swiper-button-next">
+            <Left></Left>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div> */}
+      </Swiper>
     </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        desplazarArriba: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-15%)' },
+        },
+        volverAbajo: {
+          '0%': { transform: 'translateY(-15%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        desplazarArriba: 'desplazarArriba 1s ease-in-out forwards',
+        volverAbajo: 'volverAbajo 1s ease-in-out forwards',
+      },
     },
   },
   plugins: [],
