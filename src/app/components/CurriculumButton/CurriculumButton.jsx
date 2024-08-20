fb4d153aefe7270898a1b9bdd5474d0aeecfcd0f
Mati Tari <Matiastari@outlook.com.ar>
2024-08-20 17:31:23 -0300

    Agregado de animacion de fondo con keyframes 50-149. Se agrego estilos css a boton curriculum con hover. Se corrigio imagen de Mati con mas calidad y se cambio la forma de renderizado para mejor rendimiento en la pag.

--- src/app/components/CurriculumButton/CurriculumButton.jsx
+++ src/app/components/CurriculumButton/CurriculumButton.jsx
@@ -7,12 +7,15 @@ export default function CurriculumButton() {
       <a
         href="/CurriculumMatias.pdf"
         download="Curriculum Matias Tari"
-        className="w-auto px-4  h-10 flex rounded-3xl items-center relative overflow-hidden  bg-zinc-200  py-2.5 transition-all duration-200 hover:bg-zinc-300 hover:ring-offset-2 active:ring-2 active:ring-zinc-100 active:scale-110 "
+        className="group w-auto px-4  h-10 flex rounded-3xl items-center relative overflow-hidden  bg-zinc-200  py-2.5 transition-all duration-200 hover:bg-zinc-300 hover:ring-offset-2 active:ring-2 active:ring-zinc-100 active:scale-110 "
       >
         <div className="pr-2">
           <Download />
         </div>
-        <div className="text-slate-950">Curriculum</div>
+        <span className="text-slate-950">Curriculum</span>
+        <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
+          <div className="relative h-full w-8 bg-sky-700/20"></div>
+        </div>
       </a>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,31 @@ import Carousel from './components/carousel/CarouselReact';
 import ContactForm from './components/ContactForm/ContactForm';
 
 export default function Home() {
+  const elementosParaCometas = ['1', '2', '3', '4', '5', '6', '7', '8'];
+  const cometas = [];
+  const elementosParaCometas2 = ['1', '2', '3', '4', '5', '6', '7', '8'];
+  const cometas2 = [];
+
+  for (let i = 1; i < elementosParaCometas.length; i++) {
+    cometas.push(
+      <div className={`bird b${i}`}>
+        <div className="body"></div>
+      </div>,
+    );
+  }
+  for (let i = 1; i < elementosParaCometas2.length; i++) {
+    cometas2.push(
+      <div className={`bird2 a${i}`}>
+        <div className="body2"></div>
+      </div>,
+    );
+  }
+
   return (
     <header className="w-full flex flex-col text-white">
-      {/* <div id='barNoProgress'></div>
-      <div id='barProgress'></div> */}
-      <div className="mx-auto flex md:flex-row flex-col-reverse mb-20 mt-20 md:mt-28 lg:mt-36 md:space-x-0 lg:space-x-20 w-full items-center justify-center">
+      <div className="birds front ">{cometas}</div>
+      <div className="birds2 front2 ">{cometas2}</div>
+      <div className=" mx-auto flex md:flex-row flex-col-reverse mb-20 mt-20 md:mt-28 lg:mt-36 md:space-x-0 lg:space-x-20 w-full items-center justify-center">
         <div className="flex flex-col items-center max-w-xl">
           <div className="flex justify-center">
             <div className="text-center font-normal ">
@@ -24,14 +44,22 @@ export default function Home() {
           <div className="w-11/12 p-5 text-center">
             <p className="text-xl">Bienvenido/a!</p>
             <p className="">
-              Soy graduado del bootcamp de Henry, descubrí la inmensidad del
-              mundo de la programación y su potencial para soluciones infinitas.
-              Contáctame por cualquier consulta.
+              ¡Hola! Soy Matías Tari, un desarrollador web Full Stack con una
+              sólida experiencia en diversas tecnologías. <br />
+              Manejo{' '}
+              <button className="">
+                <p className=" text-sky-600 animate-pulse text-tecno">
+                  estas tecnología
+                </p>
+              </button>{' '}
+              como si fueran extensiones de mi propio ser. Si hay un bug, lo
+              encuentro; si hay un diseño, lo mejoro; y si hay café, lo bebo. ☕️
             </p>
+            <div></div>
           </div>
           <CucrriculumButton />
         </div>
-        <div className="max-w-40 sm:max-w-60 md:max-w-80 items-center flex justify-center">
+        <div className=" z-50 max-w-40 sm:max-w-60 md:max-w-80 items-center flex justify-center">
           <Image className="img " src={MatiImage} alt="MatiImage" />
         </div>
       </div>
@@ -40,9 +68,14 @@ export default function Home() {
         <Carousel />
       </div>
       <div className="pt-10 sm:pt-5 flex justify-center flex-col text-center ">
-        <h1 className="text-xl sm:text-3xl md:text-4xl pb-14">Contactame para futuros proyectos</h1>
+        <h1 className="text-xl sm:text-3xl md:text-4xl pb-14">
+          Contactame para futuros proyectos
+        </h1>
         <ContactForm />
       </div>
     </header>
   );
+}
+function div(arg0: string): any {
+  throw new Error('Function not implemented.');
 }
